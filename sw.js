5f6de81d950c7d694596d8fdf845579c429671b1
mahmoudshaaban848-ctrl <mahmoudshaaban848@gmail.com>
2026-02-15 20:54:45 +0200

    Update service worker cache name and asset paths

--- sw.js
+++ sw.js
@@ -1,23 +1,22 @@
-const cacheName = 'azkar-v1';
+const cacheName = 'azkar-v2';
 const assets = [
-  'index.html',
-  'style.css',
-  'script.js',
-  'data.js',
-  'manifest.json',
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './data.js',
+  './manifest.json',
   'https://cdn-icons-png.flaticon.com/512/2972/2972331.png'
 ];
 
-// تثبيت الخدمة وتخزين الملفات
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
     })
   );
 });
 
-// تشغيل التطبيق من الذاكرة المخزنة
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => {
